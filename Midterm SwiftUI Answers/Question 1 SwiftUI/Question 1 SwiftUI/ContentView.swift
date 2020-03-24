4c07a64f550fa1fed5a492f77ade926e6a60b157
aditipatil138 <55366437+aditipatil138@users.noreply.github.com>
2020-03-23 21:13:03 -0700

    Add files via upload

---
+++ Midterm SwiftUI Answers/Question 1 SwiftUI/Question 1 SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Question 1 SwiftUI
+//
+//  Created by CSUFTitan on 3/23/20.
+//  Copyright © 2020 Aditi. All rights reserved.
+//
+
+//Question # 1 -----------------
+import SwiftUI
+
+struct ContentView: View {
+         var body: some View {
+           Text("Hello!")
+             .padding()
+             .background(Color.red)
+             .padding()
+             .background(Color.blue)
+             .padding()
+             .padding()
+             .background(Color.green)
+             }
+}
+
