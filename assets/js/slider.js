1dfec5c7b4322b3e5ee1310e47fbdf9d53c57b9e
aditipatil138 <55366437+aditipatil138@users.noreply.github.com>
2023-04-26 18:28:19 -0700

    Update slider.js

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,28 +1,59 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+var slider = document.getElementById("slider");
+var slideIndex = 0;
+var dots = document.getElementsByClassName("dots")[0].getElementsByTagName("span");
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function showSlides() {
+	var slides = slider.getElementsByTagName("ul")[0].getElementsByTagName("li");
+	for (var i = 0; i < slides.length; i++) {
+		slides[i].style.transform = "translateX(-" + (slideIndex * 320) + "px)"; /* 300 + 20 margin-right */
+	}
+	for (var i = 0; i < dots.length; i++) {
+		dots[i].classList.remove("active");
+	}
+	dots[slideIndex].classList.add("active");
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+showSlides();
+
+function slideNext() {
+	if (slideIndex < 4) {
+		slideIndex++;
+	} else {
+		slideIndex = 0;
+	}
+	showSlides();
 }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+function slidePrev() {
+	if (slideIndex > 0) {
+		slideIndex--;
+	} else {
+		slideIndex = 4;
+	}
+	showSlides();
 }
+
+function slideTo(index) {
+	slideIndex = index;
+	showSlides();
+}
+
+var intervalId = setInterval(slideNext, 3000);
+
+for (var i = 0; i < dots.length; i++) {
+	dots[i].addEventListener("click", function() {
+		clearInterval(intervalId);
+		slideTo(Array.prototype.indexOf.call(this.parentNode.children, this));
+		intervalId = setInterval(slideNext, 3000);
+	});
+}
+
+slider.addEventListener("mouseenter", function() {
+	clearInterval(intervalId);
+});
+
+slider.addEventListener("mouseleave", function() {
+	intervalId = setInterval(slideNext, 3000);
+});
+
+document.getElementsByClassName("prev")[0].addEventListener("
